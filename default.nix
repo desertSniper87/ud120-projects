410cd8e0fc93705cf008077f7ce5159c9772ad1e
desertsniper87 <torshobuet@gmail.com>
2019-06-03 20:48:50 +0600

    personal project files

---
+++ default.nix
@@ -0,0 +1,32 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "env";
+
+  # Mandatory boilerplate for buildable env
+  env = buildEnv { name = name; paths = buildInputs; };
+  builder = builtins.toFile "builder.sh" ''
+    source $stdenv/setup; ln -s $env $out
+  '';
+
+  # Customizable development requirements
+  buildInputs = [
+    # Add packages from nix-env -qaP | grep -i needle queries
+    # With Python configuration requiring a special wrapper
+    (python27.buildEnv.override {
+      ignoreCollisions = true;
+      extraLibs = with python27Packages; [
+        # Add pythonPackages without the prefix
+        scikitlearn
+        nltk
+        numpy
+        scipy
+        virtualenv
+      ];
+    })
+  ];
+
+  # Customizable development shell setup with at last SSL certs set
+  #shellHook = ''
+    #export SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
+  #'';
+}
