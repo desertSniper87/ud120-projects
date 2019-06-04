e9d3a9fad85c8a77a37793c21f519810efb7a699
desertsniper87 <torshobuet@gmail.com>
2019-06-05 01:44:34 +0600

    svm completion

--- choose_your_own/svm_classifier.py
+++ choose_your_own/svm_classifier.py
@@ -1,4 +1,7 @@
 import sys
+
+from time import time
+
 from class_vis import prettyPicture
 from prep_terrain_data import makeTerrainData
 
@@ -7,28 +10,43 @@ import copy
 import numpy as np
 import pylab as pl
 
+import scikitplot as skplt
+
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
-
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 ########################## SVM #################################
 ### we handle the import statement and SVC creation for you here
 from sklearn.svm import SVC
-clf = SVC(kernel="linear")
+clf = SVC(kernel="rbf", C=1000)
 
 
 #### now your job is to fit the classifier
 #### using the training features/labels, and to
 #### make a set of predictions on the test data
+time1 = time()
 clf.fit(features_train, labels_train)
+time2 = time()
+
+print ("The time to train is: ", time2-time1)
 
 #### store your predictions in a list named pred
 
+time1 = time()
 pred = clf.predict(features_test)
+time2 = time()
+
+
+print ("The time to predict is: ", time2-time1)
+
 
 
 from sklearn.metrics import accuracy_score
 acc = accuracy_score(pred, labels_test)
+
+print("The accuracy is ", acc)
 
 def submitAccuracy():
     return acc
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,19 +12,44 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+def execute(kernel_type, c_value):
+    print("We are using ", kernel_type, ", and C val is: ", c_value)
+    clf = SVC(kernel=kernel_type,
+              C=c_value)
+
+    time1 = time()
+    clf.fit(features_train, labels_train)
+    time2 = time()
+
+    print ("The time to train is: ", time2-time1)
+
+    time1 = time()
+    pred = clf.predict(features_test)
+    time2 = time()
 
 
-#########################################################
-### your code goes here ###
-
-#########################################################
+    print ("The time to predict is: ", time2-time1)
 
 
+
+    from sklearn.metrics import accuracy_score
+    acc = accuracy_score(pred, labels_test)
+
+    print("The accuracy is ", acc)
+
+    return clf
+
+# execute(kernel_type="rbf", c_value=1)
+# execute(kernel_type="rbf", c_value=10)
+# execute(kernel_type="rbf", c_value=100)
+# execute(kernel_type="rbf", c_value=1000)
+clf = execute(kernel_type="rbf", c_value=10000)
