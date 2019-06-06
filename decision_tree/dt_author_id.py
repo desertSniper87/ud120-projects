08573616e182330b83dd9ab3397fb825da396252
desertsniper87 <torshobuet@gmail.com>
2019-06-07 03:27:11 +0600

    decision tree complete

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -20,15 +20,28 @@ from sklearn.tree import DecisionTreeClassifier
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
+
+def preprocess_emails(feature_percentile):
+    print("Feature percentile is: ", feature_percentile)
+    features_train, features_test, labels_train, labels_test = preprocess(feature_percentile=feature_percentile)
+
+    print("features_train: ", len(features_train))
+    print("labels_train: ", len(labels_train))
+
+    print("features_train: ", len(features_train))
+    print("features_test: ", len(features_test))
+
+    print("Number of features in data: ", len(features_train[0]))
+
+preprocess_emails(10)
+preprocess_emails(1)
 
 
+# clf = DecisionTreeClassifier(min_samples_split=40).fit(features_train, labels_train)
+# pred = clf.predict(features_test)
 
-clf = DecisionTreeClassifier(min_samples_split=40).fit(features_train, labels_train)
-pred = clf.predict(features_test)
-
-accuracy = accuracy_score(pred, labels_test)
-print(accuracy)
+# accuracy = accuracy_score(pred, labels_test)
+# print(accuracy)
 
 
 
